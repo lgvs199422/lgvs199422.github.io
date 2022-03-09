ecd1e8a3ca516867e2d886b68429768bde504625
logovas <logovas1994@gmail.com>
2022-03-09 15:28:46 +0300

    main: added ui-kit

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,4 +1,4 @@
-
+//валидация формы
 (function () {
     let forms = document.querySelectorAll('.needs-validation')
 
@@ -14,3 +14,43 @@
             }, false)
         })
 })()
+
+//инициализация карусели
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 4,
+        dots: false,
+        margin: 32,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            },
+        },
+    });
+});
+
+//скрыть или показать пароль
+function showPassword() {
+    const eye = document.querySelector('.eye')
+    const crossedOutEye = document.querySelector('.crossed-out-eye')
+    const input = document.querySelector('.ui-input-password')
+
+    eye.addEventListener('click', () => {
+        input.setAttribute('type', 'text')
+        crossedOutEye.style.display = 'block'
+        eye.style.display = 'none'
+    })
+    crossedOutEye.addEventListener('click', () => {
+        input.setAttribute('type', 'password')
+        crossedOutEye.style.display = 'none'
+        eye.style.display = 'block'
+    })
+}
+
+showPassword()
